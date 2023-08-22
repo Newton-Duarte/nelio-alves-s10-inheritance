e4e29b6b9eaf71497bcc1eb5f987e62a930d53aa
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-08-22 06:04:15 -0300

    feat: add upcasting and downcasting examples

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Globalization;
 
-var account = new BusinessAccount(
+var bAccount = new BusinessAccount(
   123,
   "Newton Duarte",
   500,
@@ -8,12 +8,38 @@ var account = new BusinessAccount(
 );
 
 System.Console.WriteLine("Created account");
-System.Console.WriteLine(account.Balance);
+System.Console.WriteLine(bAccount.Balance);
 
 System.Console.WriteLine("Withdraw: 100");
-account.Withdraw(100);
-System.Console.WriteLine(account.Balance.ToString("F2"), CultureInfo.InvariantCulture);
+bAccount.Withdraw(100);
+System.Console.WriteLine(bAccount.Balance.ToString("F2"), CultureInfo.InvariantCulture);
 
 System.Console.WriteLine("Deposit: 200");
-account.Deposit(200);
-System.Console.WriteLine(account.Balance.ToString("F2"), CultureInfo.InvariantCulture);
+bAccount.Deposit(200);
+System.Console.WriteLine(bAccount.Balance.ToString("F2"), CultureInfo.InvariantCulture);
+
+// UPCASTING
+
+Account acc1 = bAccount;
+Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+// DOWNCASTING
+
+BusinessAccount acc4 = (BusinessAccount)acc2;
+acc4.Loan(100.0);
+
+// BusinessAccount acc5 = (BusinessAccount)acc3;
+if (acc3 is BusinessAccount) {
+    //BusinessAccount acc5 = (BusinessAccount)acc3;
+    BusinessAccount acc5 = acc3 as BusinessAccount;
+    acc5.Loan(200.0);
+    Console.WriteLine("Loan!");
+}
+
+if (acc3 is SavingsAccount) {
+    //SavingsAccount acc5 = (SavingsAccount)acc3;
+    SavingsAccount acc5 = acc3 as SavingsAccount;
+    acc5.UpdateBalance();
+    Console.WriteLine("Update!");
+}
